5a4773c5ff4cd1cfcf3f0d08a6f4cf43be32d474
harjyoti <unstoppable90p@gmail.com>
2024-05-08 19:14:05 +0530

    add

--- video-call/src/Pages/Room/Room.jsx
+++ video-call/src/Pages/Room/Room.jsx
@@ -10,8 +10,8 @@ const Room = () => {
   const myMeeting = async (element) => {
     const userID = Math.floor(Math.random() * 10000) + "";
     const userName = "userName" + userID;
-    const appID = `${import.meta.env.VITE_APP_ID}` ;
-    const serverSecret =`${import.meta.env.VITE_SERVER_SECRET}` ;
+    const appID =import.meta.env.VITE_APP_ID ;
+    const serverSecret =  import.meta.env.VITE_SERVER_SECRET;
     const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
       appID,
       serverSecret,

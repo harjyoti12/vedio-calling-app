96362f7b1b7e18cbb57a788c452fcfbb253ddd8d
harjyoti <unstoppable90p@gmail.com>
2024-05-08 19:15:26 +0530

    add

--- video-call/src/FireBase.js
+++ video-call/src/FireBase.js
@@ -8,12 +8,12 @@ import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY}`,
-  authDomain: `${import.meta.env.VITE_FIREBASE_AUTH_DOMAIN}`,
-  projectId: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}`,
-  storageBucket: `${import.meta.env.VITE_FIREBASE_STORAGE_BUCKET}`,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: `${import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID}`,
-  appId: `${import.meta.env.VITE_FIREBASE_APP_ID}`
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
